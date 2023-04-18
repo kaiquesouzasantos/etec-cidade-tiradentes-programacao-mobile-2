048ad636267f971a642421b9355bd8570f147d7b
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-04-18 15:23:08 -0300

    Add files via upload

---
+++ aulas/03/src/components/SearchBar/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { TextInput, View } from "react-native"
+import { styles } from "./style"
+
+export default function SearchBar() {
+
+    return(
+        <View style={styles.containerSearch}>
+            <TextInput 
+                placeholder="Digite o filme desejado"
+                style={styles.inputSearch}
+            >
+
+            </TextInput>
+        </View>
+    )
+}
---
+++ aulas/03/src/components/SearchBar/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    containerSearch: {
+       flexDireaction: 'row',
+       backgroundColor: 'white',
+       width: '90%',
+       marginTop: 30,
+       alignItems: 'center',
+       borderRadius: 3,  
+    },
+    inputSearch: {
+        backgroundColor: '#202020',
+        color: 'white',
+        height: 35,
+        width: '100%',
+        marginLeft: 5,
+    }
+})
+
